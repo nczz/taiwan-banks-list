a1ccadc4e3887128e4b51fe8e3f49b883de821e9
Chiang Hung Chun <im@mxp.tw>
2021-06-14 17:46:38 +0800

    update remote get file method

--- generate_json.php
+++ generate_json.php
@@ -1,4 +1,35 @@
 <?php
+// 下載 CSV 檔案
+//保留餅乾資訊
+$timeout = 10;
+$cookie  = tempnam(sys_get_temp_dir(), "mxp_");
+//假一下瀏覽器請求
+$user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36";
+//設定根層級的假請求
+ini_set("user_agent", $user_agent);
+//CURL 初始化
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
+curl_setopt($ch, CURLOPT_URL, 'https://www.cdic.gov.tw/upload/opendata/' . urlencode('要保機構名單') . '.csv');
+curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+// curl_setopt($ch, CURLOPT_ENCODING, "UTF-8");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+// curl_setopt($ch, CURLOPT_VERBOSE, true);
+$filename = '要保機構名單.csv';
+$fp       = fopen($filename, 'w');
+curl_setopt($ch, CURLOPT_FILE, $fp);
+$content  = curl_exec($ch);
+$response = curl_getinfo($ch);
+curl_close($ch);
+fclose($fp);
+// 解析 CSV 檔案
 $csv = array_map('str_getcsv', file('要保機構名單.csv'));
 // 機構類別排序
 $cats = array();
